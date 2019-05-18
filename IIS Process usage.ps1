a3b5bce8addc320df4fff0bd6f2f89699a820328
Carl Calaquian <camcalaquian+deckrobot@gmail.com>
2019-05-18 12:14:12 +0800

    Corrected line errors

--- IIS Process usage.ps1
+++ IIS Process usage.ps1
@@ -5,15 +5,10 @@ $TotalSec = (New-TimeSpan -Start $_.StartTime).TotalSeconds
 [Math]::Round( ($_.CPU * 100 / $TotalSec), 2)
 }
 }
-
-
 $processes  = Get-Process | Where{$_.ProcessName -eq "w3wp"}
 
 foreach($process in $processes)
-
 {
-
 $process | SELECT Name, $CPU
-
 }
 
